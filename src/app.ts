b4bfaf89217aa5982a0aa46e8b140ec462eceda2
lorsim <lorsim96@gmail.com>
2024-12-20 20:05:50 +0530

    updated app with routes and errorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import cors from "cors";
+import taskRoutes from "./routes/taskRoutes";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(taskRoutes);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
